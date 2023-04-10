4d2c71bc410d0807b6946e757ec7fc508bb43638
LithiumMKS <sandakov.maks@gmail.com>
2023-04-10 17:17:32 +0500

    5.3 не решил сам, не допетрил до использования словаря

--- exercises/05_basic_scripts/task_5_3.py
+++ exercises/05_basic_scripts/task_5_3.py
@@ -63,3 +63,11 @@ trunk_template = [
     "switchport mode trunk",
     "switchport trunk allowed vlan {}",
 ]
+
+mode = input('Введите режим работы интерфейса (access/trunk): ')
+interface = input('Введите тип и номер интерфейса: ')
+vlans = input('Введите номер влан(ов): ')
+template = {'access': access_template, 'trunk': trunk_template}
+
+print('\n'.join(template[mode]).format(vlans))
+
