8d9ce6e5e1e65772ae932d3c079ae671d26706e9
Lithium <sandakov.maks@gmail.com>
2023-05-08 00:52:19 +0500

    +6.2а +6.2b

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,31 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+try:
+    if ip == '0.0.0.0':
+        print('unassigned')
+    elif ip == '255.255.255.255':
+        print('local broadcast')
+    elif int(ip.split('.')[0]) in range(1, 224) \
+        and int(ip.split('.')[1]) in range(0, 256) \
+        and int(ip.split('.')[2]) in range(0, 256) \
+        and int(ip.split('.')[3]) in range(0, 256) \
+        and len(ip.split('.')) == 4:
+        print('unicast')
+    elif int(ip.split('.')[0]) in range(224, 240) \
+        and int(ip.split('.')[1]) in range(0, 256) \
+        and int(ip.split('.')[2]) in range(0, 256) \
+        and int(ip.split('.')[3]) in range(0, 256) \
+        and len(ip.split('.')) == 4:
+        print('multicast')
+    elif int(ip.split('.')[0]) in range(241, 256) \
+        and int(ip.split('.')[1]) in range(0, 256) \
+        and int(ip.split('.')[2]) in range(0, 256) \
+        and int(ip.split('.')[3]) in range(0, 256) \
+        and len(ip.split('.')) == 4:
+        print('unused')
+    else:
+        print('Неправильный IP-адрес')
+except:
+    print('Неправильный IP-адрес')
--- exercises/06_control_structures/task_6_2b.py
+++ exercises/06_control_structures/task_6_2b.py
@@ -12,3 +12,40 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+ip_correct = False
+while not ip_correct:
+    try:
+        if ip == '0.0.0.0':
+            print('unassigned')
+            break
+        elif ip == '255.255.255.255':
+            print('local broadcast')
+            break
+        elif int(ip.split('.')[0]) in range(1, 224) \
+            and int(ip.split('.')[1]) in range(0, 256) \
+            and int(ip.split('.')[2]) in range(0, 256) \
+            and int(ip.split('.')[3]) in range(0, 256) \
+            and len(ip.split('.')) == 4:
+            print('unicast')
+            break
+        elif int(ip.split('.')[0]) in range(224, 240) \
+            and int(ip.split('.')[1]) in range(0, 256) \
+            and int(ip.split('.')[2]) in range(0, 256) \
+            and int(ip.split('.')[3]) in range(0, 256) \
+            and len(ip.split('.')) == 4:
+            print('multicast')
+            break
+        elif int(ip.split('.')[0]) in range(241, 256) \
+            and int(ip.split('.')[1]) in range(0, 256) \
+            and int(ip.split('.')[2]) in range(0, 256) \
+            and int(ip.split('.')[3]) in range(0, 256) \
+            and len(ip.split('.')) == 4:
+            print('unused')
+            break
+        else:
+            print('Неправильный IP-адрес')
+            ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+    except:
+        print('Неправильный IP-адрес')
+        ip = input('Введите IP-адрес в формате 10.0.1.1: ')
