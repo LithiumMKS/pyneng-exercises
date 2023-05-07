1f566b104fc3ce857a34513d1b3fc8e8e49dfa2c
Lithium <sandakov.maks@gmail.com>
2023-05-08 00:08:07 +0500

    +6.1 +6.2

--- exercises/06_control_structures/task_6_2.py
+++ exercises/06_control_structures/task_6_2.py
@@ -12,3 +12,20 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+ip = input('Введите IP-адрес в формате 10.0.1.1: ')
+if ip == '0.0.0.0':
+    print('unassigned')
+elif ip == '255.255.255.255':
+    print('local broadcast')
+elif int(ip.split('.')[0]) in range(1, 224) \
+        and int(ip.split('.')[1]) in range(0, 256) \
+        and int(ip.split('.')[2]) in range(0, 256) \
+        and int(ip.split('.')[3]) in range(0, 256):
+    print('unicast')
+elif int(ip.split('.')[0]) in range(224, 240) \
+        and int(ip.split('.')[1]) in range(0, 256) \
+        and int(ip.split('.')[2]) in range(0, 256) \
+        and int(ip.split('.')[3]) in range(0, 256):
+    print('multicast')
+else:
+    print('unused')
