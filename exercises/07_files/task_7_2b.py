c532d3003cca55ba4aa358b8677156a9ae335175
Lithium <sandakov.maks@gmail.com>
2023-05-13 00:38:29 +0500

    +7.2b

--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -15,5 +15,24 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+from sys import argv
+
 
 ignore = ["duplex", "alias", "configuration"]
+file_input = argv[1]
+file_output = argv[2]
+
+with open(file_input) as src, open(file_output, 'w') as dst:
+    for line in src:
+        if line.startswith('!'):
+            flag = False
+        else:
+            flag = False
+            for i in ignore:
+                if line.find(i) != -1:
+                    flag = False
+                    break
+                else:
+                    flag = True
+            if flag:
+                dst.write(line)
